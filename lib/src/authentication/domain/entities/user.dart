98b9bccbffc1cd787e661a9b4307d5ea7a8a5c69
Harry Ore Lara <harryorelara@gmail.com>
2023-10-07 13:04:42 -0500

    Cargando otras opciones

--- lib/src/authentication/domain/entities/user.dart
+++ lib/src/authentication/domain/entities/user.dart
@@ -1,14 +1,15 @@
 import 'package:equatable/equatable.dart';
 
+
 class User extends Equatable {
-  final int id;
+  final String id;
   final String createdAt;
   final String name;
   final String avatar;
 
   const User.empty()
       : this(
-            id: 1,
+            id: "1",
             createdAt: '_empty_createdAt',
             name: '_empty_name',
             avatar: '_empty_avatar');
@@ -21,4 +22,5 @@ class User extends Equatable {
 
   @override
   List<Object?> get props => [id];
+
 }
